ecfb67c184be209786c526e118eb3f628c19ebfc
Ben Maharjan <benmaharjan@hotmail.co.uk>
2024-09-22 21:46:10 +0100

    more testing

--- public/main.js
+++ public/main.js
@@ -8,18 +8,21 @@ document.querySelector(".GetPlan").addEventListener("click", function() {
           return response.json();
       })
       .then(data => {
-          const resultsElement = document.querySelector('.ShowPlan');
-          resultsElement.innerHTML = ''; // Clear previous results
+
+        console.log(data);
+        
+        //   const resultsElement = document.querySelector('.ShowPlan');
+        //   resultsElement.innerHTML = ''; // Clear previous results
           
-          if (data.results && data.results.length > 0) {
-              data.results.forEach(row => {
-                  const rowElement = document.createElement('p');
-                  rowElement.textContent = JSON.stringify(row);
-                  resultsElement.appendChild(rowElement);
-              });
-          } else {
-              resultsElement.textContent = 'No workout data found.';
-          }
+        //   if (data.results && data.results.length > 0) {
+        //       data.results.forEach(row => {
+        //           const rowElement = document.createElement('p');
+        //           rowElement.textContent = JSON.stringify(row);
+        //           resultsElement.appendChild(rowElement);
+        //       });
+        //   } else {
+        //       resultsElement.textContent = 'No workout data found.';
+        //   }
       })
       .catch(error => {
           console.error('Error:', error);
